307502c4c4bde8cadc517aa51c72261dd1cbf603
Alvin Mantovani <alvin.ictn@gmail.com>
2021-08-20 19:20:32 +0700

    + fix error when importing services while where it import of service instead of services

--- src/redux/reducers/master/index.jsx
+++ src/redux/reducers/master/index.jsx
@@ -5,4 +5,4 @@ export { default as doctors } from './doctors';
 export { default as doctorSchedules } from './doctorSchedules';
 export { default as patients } from './patients';
 export { default as polis } from './polis';
-export { default as service } from './service';
+export { default as services } from './services';
