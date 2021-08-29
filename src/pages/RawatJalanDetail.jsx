9df2808fba8f44b84386a14a39a9808b98f31a71
Alvin Mantovani <alvin.ictn@gmail.com>
2021-08-29 09:33:45 +0700

    add form field on rawat jalan page

--- src/pages/RawatJalanDetail.jsx
+++ src/pages/RawatJalanDetail.jsx
@@ -8,9 +8,16 @@ const useStyles = makeStyles((theme) => ({
     },
     paper: {
       padding: theme.spacing(2),
+      margin: theme.spacing(2),
       textAlign: 'left',
       color: theme.palette.text.secondary,
+      width: '100%'
     },
+    full: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1)
+    }
   }));
   
 
@@ -21,17 +28,23 @@ export default function RawatJalanDetail() {
     return (
         <Grid container spacing={3}>
             <Grid item xs={12}>
-                <Button variant="outline" color="secondary" onClick={() => history.goBack()}>Back</Button>
+                <Button variant="outlined" color="secondary" onClick={() => history.goBack()}>Back</Button>
             </Grid>
-            <Grid item xs={6}>
-                <Paper className={classes.paper}>
-                    <Typography>Data Rawat Jalan</Typography>
-                    <TextField variant="outlined" label="No Registrasi"/>
-                </Paper>
-            </Grid>
-            <Grid item xs={6}>
-                <Paper className={classes.paper}>xs=6</Paper>
-            </Grid>
+            <Paper className={classes.paper}>
+                <Grid item xs={6}>
+                        <Typography>Data Rawat Jalan</Typography>
+                        <TextField variant="outlined" label="No Registrasi" className={classes.full}/>
+                        <TextField variant="outlined" label="NIK" className={classes.full}/>
+                        <TextField variant="outlined" label="Nama" className={classes.full}/>
+                        <TextField variant="outlined" label="Tanggal Lahir" className={classes.full}/>
+                        <TextField variant="outlined" label="Umur" className={classes.full}/>
+                        <TextField variant="outlined" label="Jenis Kelamin" className={classes.full}/>
+                    
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                </Grid>
+            </Paper>
             <Grid item xs={3}>
                 <Paper className={classes.paper}>xs=3</Paper>
             </Grid>
