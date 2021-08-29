b674cc03612ea38565bdb0c957e2a83c74681afb
Alvin Mantovani <alvin.ictn@gmail.com>
2021-08-29 08:22:35 +0700

    working on rawat jalan detail page
    + add template and form dummy

--- src/pages/RawatJalanDetail.jsx
+++ src/pages/RawatJalanDetail.jsx
@@ -1,12 +1,49 @@
+import { Button, Grid, Paper, makeStyles, Typography, TextField } from '@material-ui/core';
 import React from 'react'
 import { useHistory } from 'react-router-dom';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'left',
+      color: theme.palette.text.secondary,
+    },
+  }));
+  
+
 export default function RawatJalanDetail() {
     const history = useHistory();
+    const classes = useStyles();
+
     return (
-        <div>
-            <div onClick={() => history.goBack()}>BACk</div>
-            { history.location.pathname }
-        </div>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Button variant="outline" color="secondary" onClick={() => history.goBack()}>Back</Button>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper className={classes.paper}>
+                    <Typography>Data Rawat Jalan</Typography>
+                    <TextField variant="outlined" label="No Registrasi"/>
+                </Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper className={classes.paper}>xs=6</Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+            </Grid>
+        </Grid>
     )
 }
